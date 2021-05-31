e445b49c06dc1dd0b126a95802df700b53c6b7af
Ilya Tsyplenkov <ilya@mindsdb.com>
2021-05-31 16:17:20 +0300

    Working flow

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -30,6 +30,9 @@ STREAM_OUT_TS = f"test_stream_out_ts_{STREAM_SUFFIX}"
 DS_NAME = "kafka_test_ds"
 PREDICTOR_NAME = "kafka_test_predictor"
 
+CONNECTOR_NAME = "MindsDBConnector"
+CONNECTORS_URL = "http://127.0.0.1:9021/api/connect/connect-default/connectors"
+
 def read_stream(stream_name, buf, stop_event):
     consumer = kafka.KafkaConsumer(**CONNECTION_PARAMS, consumer_timeout_ms=1000)
     consumer.subscribe([stream_name])
@@ -81,12 +84,17 @@ class ConnectorTest(unittest.TestCase):
         upload_ds(DS_NAME)
         train_predictor(DS_NAME, PREDICTOR_NAME)
 
+    @classmethod
+    def tearDownClass(cls):
+        requests.delete(f"{CONNECTORS_URL}/{CONNECTOR_NAME}")
+
+
     def test_1_create_mindsdb_stream_via_connector(self):
         print(f'\nExecuting {self._testMethodName}')
 
-        params = {"name":"MindsDBConnector",
+        params = {"name": CONNECTOR_NAME,
                   "config": {
-                       "connector.class":"com.mindsdb.kafka.connect.MindsDBSinkConnector",
+                       "connector.class": "com.mindsdb.kafka.connect.MindsDBSinkConnector",
                        "topics": STREAM_IN,
                        "mindsdb.url": MINDSDB_URL,
                        "kafka.api.host": KAFKA_HOST,
@@ -98,13 +106,41 @@ class ConnectorTest(unittest.TestCase):
                   }
                  }
 
-        url = "http://127.0.0.1:9021/api/connect/connect-default/connectors"
-        res = requests.post(url, data=params)
-        self.assertTrue(res.status_code == 200, res.text)
+        headers = {"Content-Type": "application/json"}
+        res = requests.post(CONNECTORS_URL, json=params, headers=headers)
+        print(res.status_code)
+        print(res.text)
+        self.assertTrue(res.status_code == 201, res.text)
 
+        time.sleep(10)
         res = requests.get(f"{HTTP_API_ROOT}/config/integrations")
-        integrations = res.json()
+        self.assertTrue('integrations' in res.json(), f"Integration set is empty: {res.json()}")
+        integrations = res.json()['integrations']
         self.assertTrue(INTEGRATION_NAME in integrations, f"Can't find {INTEGRATION_NAME} in existing: {integrations}")
+
+
+    def test_2_making_stream_prediction(self):
+        print(f'\nExecuting {self._testMethodName}')
+        producer = kafka.KafkaProducer(**CONNECTION_PARAMS)
+
+        # wait when the integration launch created stream
+        time.sleep(10)
+        predictions = []
+        stop_event = threading.Event()
+        reading_th = threading.Thread(target=read_stream, args=(STREAM_OUT, predictions, stop_event))
+        reading_th.start()
+        time.sleep(1)
+
+        for x in range(1, 3):
+            when_data = {'x1': x, 'x2': 2*x}
+            to_send = json.dumps(when_data)
+            producer.send(STREAM_IN, to_send.encode("utf-8"))
+        producer.close()
+        threshold = time.time() + 30
+        while len(predictions) != 2 and time.time() < threshold:
+            time.sleep(1)
+        stop_event.set()
+        self.assertTrue(len(predictions)==2, f"expected 2 predictions but got {len(predictions)}")
 
 
 if __name__ == "__main__":
